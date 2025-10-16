bdc7fcdb71cea1ec46b91045b1d60122f3fe29c8
Divyashakti Pal <divyashaktipal@gmail.com>
2025-10-16 11:23:54 +0530

    initial commit

---
+++ Main/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About Page");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Contact Page");
+});
+
+app.listen(3000);
